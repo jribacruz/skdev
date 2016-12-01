f4b5cc69d8be66f5a55d11dc9cd900922f75816d
jribacruz <jribacruz@gmail.com>
2016-12-01 17:22:48 -0300

    ajustes no repositories

--- skdev/src/main/java/br/skdev/controller/WorkspaceController.java
+++ skdev/src/main/java/br/skdev/controller/WorkspaceController.java
@@ -2,7 +2,6 @@ package br.skdev.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
