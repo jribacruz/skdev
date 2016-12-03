0a595138b879d663b4b04b71b943a6ddfa9be9ea
jribacruz <jribacruz@gmail.com>
2016-12-02 21:59:33 -0300

    InputString

--- skdev/src/main/java/br/skdev/core/component/InputString.java
+++ skdev/src/main/java/br/skdev/core/component/InputString.java
@@ -8,15 +8,25 @@ import br.skdev.core.UIComponent;
  * @author jcruz
  *
  */
-public class Input extends UIComponent {
+public class InputString extends UIComponent {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public Input(String id) {
+	private String value;
+
+	public InputString(String id) {
 		super(id);
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
 	}
 
 }
