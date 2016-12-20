164fbb13f818b76f87865809ac90fc4fae66e372
jribacruz <jribacruz@gmail.com>
2016-12-20 15:38:58 -0300

    remoção de Método

--- skdev/src/main/java/br/skdev/core/service/ProjectService.java
+++ skdev/src/main/java/br/skdev/core/service/ProjectService.java
@@ -1,12 +1,8 @@
 package br.skdev.core.service;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Optional;
 import java.util.SortedSet;
 
-import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,22 +41,6 @@ public class ProjectService {
 	 */
 	public Optional<EMavenProject> findByName(String projectName) {
 		return projectRepository.findByName(projectName);
-	}
-
-	/**
-	 * 
-	 * @param mavenProject
-	 * @param path
-	 * @throws IOException
-	 */
-	public void createDir(EMavenProject mavenProject, String path) throws IOException {
-		String npath = FilenameUtils.normalize(String.format("%s/%s", mavenProject.getAbsolutePath(), path));
-		if (Files.notExists(Paths.get(npath))) {
-			log.info("Criando diretório: {}", npath);
-			Files.createDirectories(Paths.get(path));
-			return;
-		}
-		log.info("Diretório já existe: {}", npath);
 	}
 
 }
