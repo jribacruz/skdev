1eb510d3a520b0aebb6ac6f4f9a75666c1ff6f02
jribacruz <jribacruz@gmail.com>
2016-12-22 09:01:09 -0300

    ajuste nos services

--- skdev/src/main/java/br/skdev/core/rest/ProjectRest.java
+++ skdev/src/main/java/br/skdev/core/rest/ProjectRest.java
@@ -4,9 +4,9 @@ import java.util.SortedSet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.skdev.core.context.WorkspaceContext;
@@ -39,8 +39,8 @@ public class ProjectRest {
 	 * @return
 	 * @throws InterruptedException
 	 */
-	@RequestMapping(method = RequestMethod.GET, path = "/api/project/classes")
-	public ResponseEntity<?> findAllEClasses(@RequestParam("name") String projectName) {
+	@RequestMapping(method = RequestMethod.GET, path = "/api/projects/{projectName}/classes")
+	public ResponseEntity<?> findAllEClasses(@PathVariable("projectName") String projectName) {
 		SortedSet<EClass> entities = projectService.findAllClasses(projectName);
 		return ResponseEntity.ok(entities);
 
