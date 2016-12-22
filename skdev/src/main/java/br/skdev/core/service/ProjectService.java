be8635da93e5ad8eec8d93f726c68a31a827a68b
jribacruz <jribacruz@gmail.com>
2016-12-22 09:44:51 -0300

    refactor

--- skdev/src/main/java/br/skdev/core/service/ProjectService.java
+++ skdev/src/main/java/br/skdev/core/service/ProjectService.java
@@ -8,11 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.skdev.core.model.EClass;
-import br.skdev.core.model.EMavenFolder;
 import br.skdev.core.model.EMavenProject;
-import br.skdev.core.model.EPackage;
 import br.skdev.core.repository.ProjectRepository;
-import br.skdev.core.util.FS;
 
 /**
  * 
@@ -24,9 +21,6 @@ public class ProjectService {
 
 	@Autowired
 	private ProjectRepository projectRepository;
-
-	@Autowired
-	private FS fs;
 
 	private Logger log = LoggerFactory.getLogger(ProjectService.class);
 
@@ -41,38 +35,11 @@ public class ProjectService {
 
 	/**
 	 * 
-	 * @param projectName
+	 * @param eMavenProject
 	 * @return
 	 */
 	public SortedSet<EClass> findAllEClasses(EMavenProject eMavenProject) {
 		return projectRepository.findAllEClasses(eMavenProject);
-	}
-
-	/**
-	 * Cria um diretório no projeto.
-	 * 
-	 * @param eMavenProject
-	 * @param eMavenFolder
-	 * @param path
-	 */
-	public void mkdir(EMavenProject eMavenProject, EMavenFolder eMavenFolder, String path) {
-		String finalPath = fs.normalize(eMavenProject.getAbsolutePath(), eMavenFolder.getPath(), path);
-		log.info("[mkdir] path={}", finalPath);
-		fs.mkdir(finalPath);
-	}
-
-	/**
-	 * Cria um diretório no projeto.
-	 * 
-	 * @param eMavenProject
-	 * @param mavenFolder
-	 * @param ePackage
-	 * @param path
-	 */
-	public void mkdir(EMavenProject eMavenProject, EMavenFolder eMavenFolder, EPackage ePackage, String path) {
-		String finalPath = fs.normalize(eMavenProject.getAbsolutePath(), eMavenFolder.getPath(), ePackage.getDirectory(), path);
-		log.info("[mkdir] path={}", finalPath);
-		fs.mkdir(finalPath);
 	}
 
 }
