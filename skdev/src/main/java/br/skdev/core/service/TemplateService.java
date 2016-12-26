21b4f9197430587708ed20cab30187c24e1e2aa3
jribacruz <jribacruz@gmail.com>
2016-12-26 14:52:58 -0300

    ajuste nos metodos do service

--- skdev/src/main/java/br/skdev/core/service/TemplateService.java
+++ skdev/src/main/java/br/skdev/core/service/TemplateService.java
@@ -1,6 +1,10 @@
 package br.skdev.core.service;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.StringReader;
+import java.io.Writer;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +13,7 @@ import org.springframework.stereotype.Service;
 import br.skdev.core.model.ETemplate;
 import br.skdev.core.model.ETemplateModel;
 import br.skdev.core.repository.TemplateRepository;
-import br.skdev.core.template.Template;
+import freemarker.template.Configuration;
 import freemarker.template.TemplateException;
 
 @Service
@@ -18,14 +22,20 @@ public class TemplateService {
 	@Autowired
 	private TemplateRepository templateRepository;
 
-	@Autowired
-	private Template template;
-
 	public ETemplate findByName(String name) {
 		return templateRepository.findByName(name);
 	}
 
 	public String merge(ETemplate etemplate, Map<String, ETemplateModel> models) throws TemplateException, IOException {
-		return template.merge(etemplate.getContent(), models);
+		return merge(etemplate.getContent(), models);
+	}
+
+	public String merge(String template, Map<String, ETemplateModel> model) throws TemplateException, IOException {
+		freemarker.template.Template inlineTemplate = new freemarker.template.Template("inlineTemplate", new StringReader(template),
+				new Configuration());
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		Writer out = new OutputStreamWriter(outputStream);
+		inlineTemplate.process(model, out);
+		return outputStream.toString();
 	}
 }
