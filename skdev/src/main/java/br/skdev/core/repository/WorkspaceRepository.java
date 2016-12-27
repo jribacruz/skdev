fd9077b922ec3ce2cffda391ebccc2ae637c3153
jribacruz <jribacruz@gmail.com>
2016-12-27 08:54:47 -0300

    remoção de métodos

--- skdev/src/main/java/br/skdev/core/repository/WorkspaceRepository.java
+++ skdev/src/main/java/br/skdev/core/repository/WorkspaceRepository.java
@@ -2,15 +2,9 @@ package br.skdev.core.repository;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import br.skdev.core.exception.WorkspaceNotFoundException;
@@ -21,35 +15,6 @@ import br.skdev.core.proxy.EWorkspaceProxy;
 public class WorkspaceRepository {
 
 	private Logger log = LoggerFactory.getLogger(WorkspaceRepository.class);
-
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
-
-
-	public EWorkspace findWorkspace() {
-		String q = "SELECT * FROM WORKSPACE W";
-		try {
-			return jdbcTemplate.queryForObject(q, new RowMapper<EWorkspace>() {
-				@Override
-				public EWorkspace mapRow(ResultSet rs, int arg1) throws SQLException {
-					return new EWorkspaceProxy(rs.getString("path"));
-				}
-			});
-		} catch (EmptyResultDataAccessException e) {
-			log.info("Nenhum workspace encontrado");
-		}
-		return null;
-	}
-
-	public int updateWorkspace(String path) {
-		String q = "UPDATE WORKSPACE SET PATH=? WHERE PATH=?";
-		return jdbcTemplate.update(q, path, path);
-	}
-
-	public int insertWorkspace(String path) {
-		String q = "INSERT INTO WORKSPACE(PATH) VALUES(?)";
-		return jdbcTemplate.update(q, path);
-	}
 
 	public EWorkspace load(String path) {
 		if (Files.notExists(Paths.get(path))) {
