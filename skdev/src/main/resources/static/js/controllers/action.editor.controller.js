9705c3d9ae7b3ed77d54ef9020d3a65c1573d0f4
jribacruz <jribacruz@gmail.com>
2017-01-06 11:02:45 -0300

    ajuste no action editor

--- skdev/src/main/resources/static/js/controllers/action.editor.controller.js
+++ skdev/src/main/resources/static/js/controllers/action.editor.controller.js
@@ -22,29 +22,40 @@
 		var editors = {};
 
 		init();
-		
+
 		function init() {
 			$log.debug('[ActionEditorCT] Inicializando editor execute.js')
 			angular.element(document).ready(function() {
-				//console.log(document.getElementById('executeEditor'))
-				editors['execute'] = CodeMirror(document.getElementById('executeEditor'),{
-					  value: "$notification.show('Funcionou')",
-					  mode:  "javascript",
-					  lineNumbers: true,
-					  gutters: ["CodeMirror-lint-markers"],
-					  lint: true,
-					  autoCloseBrackets: true,
-					  extraKeys: {"Ctrl-Space": "autocomplete"},
-					  matchBrackets: false,
-					  theme: 'eclipse',
-					  indentUnit: 4
-				});	
-				editors['dialog'] = CodeMirror(document.getElementById('dialogEditor'),{
-					  mode:  "htmlmixed",
-					  lineNumbers: true,
-					  autoCloseTags: true,
-					  theme: 'eclipse'
-					});
+				/*
+				 * Inicializando o editor de dialog.
+				 */
+				editors['dialog'] = CodeMirror(document.getElementById('dialogEditor'), {
+					mode : "htmlmixed",
+					lineNumbers : true,
+					autoCloseTags : true,
+					theme : 'eclipse'
+				});
+				editors['dialog'].setSize('100%', '100%');
+				
+				/*
+				 * Inicializando o editor de action execute
+				 */
+				editors['execute'] = CodeMirror(document.getElementById('executeEditor'), {
+					value : "$notification.show('Funcionou')",
+					mode : "javascript",
+					lineNumbers : true,
+					gutters : [ "CodeMirror-lint-markers" ],
+					lint : true,
+					autoCloseBrackets : true,
+					extraKeys : {
+						"Ctrl-Space" : "autocomplete"
+					},
+					matchBrackets : false,
+					theme : 'eclipse',
+					indentUnit : 4
+				});
+				editors['execute'].setSize('100%', '100%');
+
 			});
 		}
 	}
