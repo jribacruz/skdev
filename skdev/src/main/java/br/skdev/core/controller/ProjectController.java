8c4b3d18e919cc0b8b03d4ea95d97d94aca82ba4
jribacruz <jribacruz@gmail.com>
2017-01-10 14:08:37 -0300

    ajuste no core

--- skdev/src/main/java/br/skdev/core/controller/ProjectController.java
+++ skdev/src/main/java/br/skdev/core/controller/ProjectController.java
@@ -2,10 +2,7 @@ package br.skdev.core.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import br.skdev.core.service.ProjectService;
 
@@ -20,13 +17,6 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 
-	@RequestMapping(method = RequestMethod.GET, path = "/project")
-	public String index(@RequestParam("name") String name, Model model) {
-		model.addAttribute("project", name);
-		// workspaceContext.setMavenProject(projectService.findByName(name));
-		return "project";
-	}
-	
 	@RequestMapping("/workspace")
 	public String projects() {
 		return "projects/index";
