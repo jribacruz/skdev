a76561bba31a9aea287248ff54e6b2104c56f04b
jribacruz <jribacruz@gmail.com>
2017-01-17 09:39:54 -0300

    remoção de comentário

--- skdev/src/main/java/br/skdev/core/repository/FragmentRepository.java
+++ skdev/src/main/java/br/skdev/core/repository/FragmentRepository.java
@@ -1,7 +1,6 @@
 package br.skdev.core.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import br.skdev.core.domain.Fragment;
 
